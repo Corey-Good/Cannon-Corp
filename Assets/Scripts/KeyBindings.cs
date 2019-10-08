49a93556d06c4a797edeaa2a403d0c1477e8612e
Corey-Good <coreygood6@gmail.com>
2019-10-08 18:58:43 -0500

    Added key bindings

---
+++ Assets/Scripts/KeyBindings.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyBindings : MonoBehaviour
+{
+    public static string forwardKey = "W";
+    public static string backwardKey = "S";
+    public static string leftKey = "A";
+    public static string rightKey = "D";
+    public Text forwardButton;
+    public Text backwardButton;
+    public Text leftButton;
+    public Text rightButton;
+    private string keyHit;
+    public bool lookingForKey = false;
+    private string objectName;
+
+    private void Start()
+    {
+        forwardButton.text = forwardKey;
+        backwardButton.text = backwardKey;
+        leftButton.text = leftKey;
+        rightButton.text = rightKey;
+    }
+
+    public void OnClick(Text text)
+    {
+        text.text = "Hit Key";
+        lookingForKey = true;
+        objectName = text.name;           
+    }
+
+    public void OnGUI()
+    {
+        Event e = Event.current;
+        if (e.isKey && lookingForKey)
+        {
+            keyHit = e.keyCode.ToString();
+            switch (objectName)
+            {
+                case "ForwardText":
+                    forwardButton.text = keyHit;
+                    forwardKey = forwardButton.text;
+                    break;
+                case "BackwardText":
+                    backwardButton.text = keyHit;
+                    backwardKey = backwardButton.text;
+                    break;
+                case "LeftText":
+                    leftButton.text = keyHit;
+                    leftKey = leftButton.text;
+                    break;
+                case "RightText":
+                    rightButton.text = keyHit;
+                    rightKey = rightButton.text;
+                    break;
+            }
+            lookingForKey = false;
+        }
+    }
+
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,14 +7,61 @@ public class PlayerMovement : MonoBehaviour
     private float movementForce = (float)CharacterInfo.info[CharacterMenu.currentModelIndex]["movementForce"];
     private float rotateSpeed = (float)CharacterInfo.info[CharacterMenu.currentModelIndex]["rotationSpeed"];
 
+    private string forwardbutton;
+    private string backwardbutton;
+    private string leftbutton;
+    private string rightbutton;
+
+    private void Start()
+    {
+        if(Equals(KeyBindings.forwardKey, "UpArrow"))
+        {
+            forwardbutton = "up";
+        }
+        else
+        { 
+            forwardbutton = KeyBindings.forwardKey.ToLower();
+        }
+
+        if (Equals(KeyBindings.backwardKey, "DownArrow"))
+        {
+            backwardbutton = "down";
+        }
+        else
+        {
+            backwardbutton = KeyBindings.backwardKey.ToLower();
+        }
+
+        if (Equals(KeyBindings.leftKey, "LeftArrow"))
+        {
+            leftbutton = "left";
+        }
+        else
+        {
+            leftbutton = KeyBindings.leftKey.ToLower();
+        }
+
+        if (Equals(KeyBindings.rightKey, "RightArrow"))
+        {
+            rightbutton = "right";
+        }
+        else
+        {
+            rightbutton = KeyBindings.rightKey.ToLower();
+        }
+
+
+
+
+    }
     void FixedUpdate()
     {
         // Move play forwards and backwards, regenerate health when no movement is detected
-        if (Input.GetKey("w"))
+        if (Input.GetKey(forwardbutton))
         {
             transform.position += transform.forward * Time.deltaTime * movementForce;
         } 
-        else if (Input.GetKey("s"))
+        else if (Input.GetKey(backwardbutton))
         {
             transform.position += -transform.forward * Time.deltaTime * movementForce;
         } 
@@ -24,11 +71,11 @@ public class PlayerMovement : MonoBehaviour
         }
 
         // Rotate model left and right
-        if (Input.GetKey("d"))
+        if (Input.GetKey(rightbutton))
         {
             transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
         }
-        else if (Input.GetKey("a"))
+        else if (Input.GetKey(leftbutton))
         {
             transform.Rotate(-Vector3.up * rotateSpeed * Time.deltaTime);
         }
@@ -38,17 +85,6 @@ public class PlayerMovement : MonoBehaviour
         {
             LoadUI.currentHealth -= 1.0f;
         }
-
-
-
-
-
-
-
-
-
-
-
 
     }
 
