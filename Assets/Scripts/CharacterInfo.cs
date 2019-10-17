42b0854b7032d7f7b43d16469c2712d43191a0af
Corey-Good <coreygood6@gmail.com>
2019-10-17 11:00:42 -0500

    Added rotating tanks with newest models

--- Assets/Scripts/CharacterInfo.cs
+++ Assets/Scripts/CharacterInfo.cs
@@ -19,7 +19,7 @@ public class CharacterInfo : MonoBehaviour
             { // Model 1, this is the base model tank
                 0, new Hashtable()
                 {
-                    {"modelCameraOffset", new Vector3(0.0f, 4.0f, -12.6f)},
+                    {"modelCameraOffset", new Vector3(0.0f, 3.5f, -9.0f)},
                     {"movementForce", 7.0f },
                     {"rotationSpeed", 30.0f },
                     {"healthPoints", 65.0f},
@@ -33,7 +33,7 @@ public class CharacterInfo : MonoBehaviour
             { // Model 2, this is the futuristic tank
                 1, new Hashtable()
                 {
-                    {"modelCameraOffset", new Vector3(0.0f, 1.2f, -15.0f)},
+                    {"modelCameraOffset", new Vector3(0.0f, 2.5f, -10.0f)},
                     {"movementForce", 10.0f },
                     {"rotationSpeed", 40.0f },
                     {"healthPoints", 55.0f},
@@ -46,7 +46,7 @@ public class CharacterInfo : MonoBehaviour
             { // Model 3, this is the catapult
                 2, new Hashtable()
                 {
-                    {"modelCameraOffset", new Vector3(0.0f, 9.6f, -27.5f)},
+                    {"modelCameraOffset", new Vector3(0.0f, 3.5f, -9.0f)},
                     {"movementForce", 5.0f },
                     {"rotationSpeed", 25.0f },
                     {"healthPoints", 90.0f},
@@ -59,7 +59,7 @@ public class CharacterInfo : MonoBehaviour
             { // Model 4, this is the Cartoon Tank
                 3, new Hashtable()
                 {
-                    {"modelCameraOffset", new Vector3(0.0f, 5.0f, -32.5f)},
+                    {"modelCameraOffset", new Vector3(0.0f, 3.5f, -26.4f)},
                     {"movementForce", 8.0f },
                     {"rotationSpeed", 35.0f },
                     {"healthPoints", 65.0f},
@@ -72,7 +72,7 @@ public class CharacterInfo : MonoBehaviour
             { // Model 5, this is the box tank
                 4, new Hashtable()
                 {
-                    {"modelCameraOffset", new Vector3(0.0f, 2.2f, -7.2f)},
+                    {"modelCameraOffset", new Vector3(0.0f, 3.5f, -9.0f)},
                     {"movementForce", 6.0f },
                     {"rotationSpeed", 30.0f },
                     {"healthPoints", 100.0f},
