0979ac0b14b06578c835e03e5b81b0cc721c8db1
Corey-Good <coreygood6@gmail.com>
2019-10-26 15:11:06 -0500

    Merge further scripts

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -19,6 +19,32 @@ public class PlayerMovement : MonoBehaviourPun
     private string rightbutton;
 
 
+
+    public GameObject bullet;              // Contains the bullet model
+    private GameObject bulletCopy;          // Copy of the bullet that gets launched
+    public GameObject bulletSpawnLocation; // Where the bull is instantiated
+    private Rigidbody bulletRB;            // Rigibody of the bullet to be launched
+
+    private float timeElapsed = 0f;
+    private bool bulletActive = false;
+    private float reloadSpeed = (float)CharacterInfo.info[CharacterMenu.currentModelIndex]["reloadSpeed"];
+    private float bulletSpeed = (float)CharacterInfo.info[CharacterMenu.currentModelIndex]["bulletSpeed"];
+    private float bulletArch = (float)CharacterInfo.info[CharacterMenu.currentModelIndex]["bulletArch"];
+    public static float reloadProgress;
+
+
+    private void Awake()
+    {
+        // Ensures the bar is full when starting the game
+        reloadProgress = 1.0f;
+    }
+
+
+
+
+
+
+
     void FixedUpdate()
     {
         SetKeyBindings();
@@ -33,6 +59,32 @@ public class PlayerMovement : MonoBehaviourPun
             else
             {
                 MoveXNormal();
+            }
+        }
+
+        if (Input.GetMouseButtonDown(KeyBindings.clickIndex) && bulletActive == false && !PauseMenu.GameIsPaused)
+        {
+            // Creates a copy of the bullet, and captures its Rigibody (into bulletRB)
+            bulletCopy = PhotonNetwork.Instantiate("Bullet", bulletSpawnLocation.transform.position, Quaternion.Euler(0, 0, 0));
+            bulletRB = bulletCopy.GetComponent<Rigidbody>();
+
+            // Applies the launching force to the bullet and sets its status to active (true)
+            bulletRB.AddRelativeForce(((transform.forward * bulletSpeed) + (transform.up * bulletArch)), ForceMode.Impulse);
+            bulletActive = true;
+        }
+
+        if (bulletActive)
+        {
+            // Increase time and update the reloadBar progress
+            timeElapsed += Time.deltaTime;
+            reloadProgress = timeElapsed / reloadSpeed;
+
+            // When a bullet is reloaded, delete the previouis copy and reset timer
+            if (timeElapsed >= reloadSpeed)
+            {
+                PhotonNetwork.Destroy(bulletCopy);
+                timeElapsed = 0f;
+                bulletActive = false;
             }
         }
     }
