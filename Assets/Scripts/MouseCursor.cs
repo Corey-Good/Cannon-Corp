20d0c45b8c88b05bf719122e6227375efb256bfe
Corey-Good <coreygood6@gmail.com>
2019-11-17 13:03:05 -0600

    Added crosshairs and new statue

---
+++ Assets/Scripts/MouseCursor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCursor : MonoBehaviour
+{
+
+    private Vector3 mousePos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = Input.mousePosition;
+        mousePos.z = 1.0f;
+        Vector3 cursorPos = Camera.main.ScreenToWorldPoint(mousePos);
+        transform.position = cursorPos;
+    }
+}
