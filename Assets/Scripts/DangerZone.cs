e4fd18a50d6050b2c80bf0da2740272ac6fce136
Corey-Good <coreygood6@gmail.com>
2019-11-19 20:06:11 -0600

    Update

--- Assets/Scripts/DangerZone.cs
+++ Assets/Scripts/DangerZone.cs
@@ -7,8 +7,18 @@ public class DangerZone : MonoBehaviourPun
     public GameObject dangerMessage;
     // Start is called before the first frame update
 
+    public void Awake()
+    {
+        if (!photonView.IsMine)
+        {
+            GameObject danger = GameObject.FindWithTag("DangerZone");
+            danger.SetActive(false);
+        }
+    }
     private void OnTriggerExit(Collider other)
     {
+
+        Debug.Log(other.gameObject.tag);
         if (other.gameObject.tag == "Player")
         {
             isInDanger = true;
@@ -18,8 +28,7 @@ public class DangerZone : MonoBehaviourPun
 
     private void OnTriggerEnter(Collider other)
     {
-        if (!photonView.IsMine) return;
-
+        Debug.Log(other.gameObject.tag);
         if (other.gameObject.tag == "Player")
         {
             isInDanger = false;
