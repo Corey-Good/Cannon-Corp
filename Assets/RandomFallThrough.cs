74f88a5d68fd9c0dcfe73933c75809bf7ac22118
Corey-Good <55670356+Corey-Good@users.noreply.github.com>
2019-11-25 11:43:32 -0600

    Updates

--- Assets/RandomFallThrough.cs
+++ Assets/RandomFallThrough.cs
@@ -5,20 +5,17 @@ using UnityEngine;
 public class RandomFallThrough : MonoBehaviour
 {
 
-    private void OnCollisionEnter(Collision collisionInfo)
+    private void OnCollisionEnter(Collision collision)
     {
         if (Random.Range(0, 5) == 0)
         {
             GetComponent<BoxCollider>().enabled = false;
             Debug.Log("Fallthrough!");
         }
+        else
+        {
+            GetComponent<BoxCollider>().enabled = true;
+            Debug.Log("Turn off fallthrough!");
+        }
     }
-
-    private void OnCollisionExit(Collision collisionInfo)
-    {
-        GetComponent<BoxCollider>().enabled = true;
-    }
-
- 
-
 }
--- Assets/SpringBoard.cs
+++ Assets/SpringBoard.cs
@@ -8,10 +8,8 @@ public class SpringBoard : MonoBehaviour
     {
         if (Random.Range(0, 3) == 0)
         {
-            collisionInfo.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * 10000, ForceMode.Acceleration);
+            collisionInfo.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * 5000, ForceMode.Acceleration);
             Debug.Log("Launch!!!");
         }
     }
-
-
 }
