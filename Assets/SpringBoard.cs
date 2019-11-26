7500e455a2847648c03da0c9c43382e38ebeaa53
Corey-Good <coreygood6@gmail.com>
2019-11-26 09:36:05 -0600

    Fine tuned the mechanics for the obstacle course

--- Assets/SpringBoard.cs
+++ Assets/SpringBoard.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class SpringBoard : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collisionInfo)
+    private void OnCollisionStay(Collision collisionInfo)
     {
-        if (Random.Range(0, 3) == 0)
+        int randomNumber = Random.Range(0, 115);
+        float direction;
+        if(Random.Range(0,2) == 0)
         {
-            collisionInfo.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * 5000, ForceMode.Acceleration);
-            Debug.Log("Launch!!!");
+            direction = 0.75f;
+        }
+        else
+        {
+            direction = -0.5f;
+        }
+        
+        if (randomNumber == 0)
+        {
+            collisionInfo.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, 1, direction) * 2500, ForceMode.Impulse);
+          
         }
     }
 }
