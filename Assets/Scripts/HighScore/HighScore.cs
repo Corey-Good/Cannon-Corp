6eb4849267d49f154c87c4af09462f312a942ea5
MediocrePick <55897070+MediocrePick@users.noreply.github.com>
2019-11-30 22:44:17 -0600

    sent message over debug.log

--- Assets/Scripts/HighScore/HighScore.cs
+++ Assets/Scripts/HighScore/HighScore.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
 public class HighScore : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Awake()
     {
-        
+        PhotonView photonView = PhotonView.Get(this);
+        photonView.RPC("ChatMessage", RpcTarget.All, "jup", "and jup!");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        ChatMessage("test1", "test2");
+    }
+    // Start is called before the first frame update
+    [PunRPC]
+    void ChatMessage(string a, string b)
+    {
+        Debug.Log(string.Format("ChatMessage {0} {1}", a, b));
     }
 }
